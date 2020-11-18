c7c6c44fddb25c0407e788a695cea96c1d8924b2
Mazhar Ali Baig <2018145@iiitdmj.ac.in>
2020-11-18 15:52:47 +0530

    connected to DB, CRUD not updated

--- controllers/todoController.js
+++ controllers/todoController.js
@@ -1,20 +1,68 @@
+var mongoose = require('mongoose');
+
+var data = [{item: 'Task no.1'},{item: 'Task no.2'},{item: 'Task no.3'}];
+var bodyParser = require('body-parser');
+
+mongoose.connect('mongodb+srv://mazhar_ali_baig:Maryam09@cluster0.zvl25.mongodb.net/todo-database?retryWrites=true&w=majority',{useNewUrlParser: true});
+
+// Constructing Schema
+
+const todoSchema = new mongoose.Schema({
+
+    item: String
+
+});
+
+var Todo = mongoose.model('Todo', todoSchema);
+
+var itemOne = Todo({item: "Buy me them flowers bro!"}).save((err) => {
+
+    if(err)
+        throw err;
+
+    else
+        console.log("Item saved!");
+
+})
+
+var urlencodedParser = bodyParser.urlencoded({ extended: true });
+
+// console.log(data);
+
 module.exports = function(app){
 
 app.get('/todo', (req,res) => {
+
+    // console.log(data);
     
-    res.render('todo');
+    res.render('todo',{todos: data});
 
 });
 
-app.post('/todo',(req,res) => {
-    
-    res.end('It Works!!');
+app.post('/todo',urlencodedParser,(req,res) => {
+
+    data.push(req.body);
+
+    res.render('todo', {todos: data});
 
 });
 
-app.delete('/todo',(req,res) => {
-    
-    res.end('It Works!!');
+app.delete('/todo/:item',(req,res) => {
+
+    data = data.filter((todo) => {
+
+        console.log(todo.item.replace(/ /g,"-"));
+
+        console.log(req.params.item);
+
+        return todo.item.trim().replace(/ /g,"-") !== req.params.item;
+
+    });
+
+    console.log('Updated List!!')
+    console.log(data);
+
+    res.render('todo', {todos: data});
 
 });
 
--- public/assets/todo-list.js
+++ public/assets/todo-list.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
 
   $('form').on('submit', function(){
 
-      var item = $('form input');
-      var todo = {item: item.val()};
+      var x = $('form input');
+      var todo = {item: x.val()};
 
       $.ajax({
         type: 'POST',
@@ -20,7 +20,10 @@ $(document).ready(function(){
   });
 
   $('li').on('click', function(){
-      var item = $(this).text().replace(/ /g, "-");
+
+    // console.log("Clicked!!");
+
+      var item = $(this).text().trim().replace(/ /g, "-");
       $.ajax({
         type: 'DELETE',
         url: '/todo/' + item,
